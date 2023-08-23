51748e19dc4f48cf4e84dad7e8a721ca60ea92d6
polina-tsvilodub <polina.tsvilodub@gmail.com>
2023-08-23 16:28:42 +0200

    deploy to prolific

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -3,9 +3,9 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev//',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev//socket',
   // this will be used in prolific mode
-  completionUrl: 'https://...',
+  completionUrl: 'https://app.prolific.co/submissions/complete?cc=CT3H31V4',
   contactEmail: 'polina.tsvilodub@uni-tuebingen.de',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };
